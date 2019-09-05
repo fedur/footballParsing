1dafc02e7e0e06468a8ca32b38fade6899fe54c0
felix.durand.p <felix.durand.p@gmail.com>
2019-09-05 16:08:39 -0400

    Run and pass plays currently managed, and parsed

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ import re
 #to-do list, handle quarters
 
 PLAY_NB = 1
+def getAbsFieldPos(teamWithBall,side,yd):
+    if side == teamWithBall:
+        return yd
+
+    else:
+        return 55+55-yd
+
 def processHeader(header,play, t1, t2):
     #format of the header: S 1-10 S39
     #res[0] = Team with ball
@@ -13,6 +20,8 @@ def processHeader(header,play, t1, t2):
     rgx = re.compile("\s+")
     res = rgx.split(header)
     teamWithBall = res[0]
+    downAndDistance = res[1].split('-')
+    fieldPos = res[2]
     if t1[0] == teamWithBall:
         play["off_team"] = t1
         play["def_team"] = t2
@@ -21,28 +30,72 @@ def processHeader(header,play, t1, t2):
         play["off_team"] = t2
         play["def_team"] = t1
 
-    downAndDistance = res[1].split('-')
+
     play["down_nb"] = downAndDistance[0]
     play["distance_to_first"] = downAndDistance[1]
+    play["at_yard"]=getAbsFieldPos(
+        teamWithBall,
+        fieldPos[0],
+        (int(fieldPos[1] + fieldPos[2]))
+        )
 
-    fieldPos=res[2]
-    side=fieldPos[0]
-    yd=(int(fieldPos[1] + fieldPos[2]))
-    if side == teamWithBall:
-        play["at_yard"] = yd
+def processPassPlay(text,play):
+    #format: QBName pass complete|incomplete to RECEIVER for x yards
+    match=re.search("\spass\s(complete|incomplete)\sto\s(.*)",text, re.DOTALL)
+    if match != None:
+        play["play_type"]="pass"
+        playType=match.groups()[0] #complete / incomplete
+        play["play_result"]=playType
+        #Format: Adam Vance pass complete to Jeremy Murphy for 43 yards to the MTL37,
+        # (Tackler Name; Tackler2 name)
+        if playType == "complete":
+            match2 = \
+                re.search("(.*?)\sfor\s(\d+)\syards\sto\sthe\s(\w*)\s.*?\((.*?)\)",
+                    match.groups()[1],
+                    re.DOTALL)
+            if match2 != None:
+                #1: Receiver, 2: Gain, 3: FieldPos, 4: Tackler(s)
+                play["thrown_to"]=match2.group(1)
+                play["gain"]=match2.group(2)
+                # Some tackler names overlap on multiple lines...
+                play["tackled_by"]=re.sub("\n\s*"," ",match2.group(4))
 
+        #
+        elif playType =="incomplete":
+            receiver = re.match("(.*?\s.*?)[\s\.,]",
+                match.groups()[1])
+            if (receiver != None):
+                play["thrown_to"]=receiver.groups()[0]
+        return True
     else:
-        play["at_yard"] = 55+55-yd
+        return False
+
+def processRunPlay(text,play):
+    match=re.match("(.*?)\srush\sfor\s((?:loss\sof\s)?\d+)",text, re.DOTALL)
+    if match != None:
+        #1: Name of player, 2: (loss of) x
+        play["run_by"]=match.group(1).strip()
+        if match.group(2)[0] == 'l':
+            yd=re.match(".*(\d+)",match.group(2))
+            if yd != None:
+                val=int(yd.group(1))
+                val=val*-1
+                play["gain"]=val
+        else:
+            play["gain"]=int(match.group(2))
+
+        print(play["run_by"] + ", "+str(play["gain"]))
+        
+    return True
 
 def processPlay(text,play):
-    if re.search("\spass\s",text,re.DOTALL) != None:
-        #format: QBName pass complete|incomplete to RECEIVER for x yards 
-        reg=re.compile("\spass\s(complete|incomplete)\sto\s(.*)", re.DOTALL)
-        match=reg.search(text)
-        print(match.groups())
+    play["penalty_on_play"] = "true" \
+        if re.search("PENALTY",text, re.DOTALL) \
+        else "false"
 
-    elif re.search("\srush\s",text, re.DOTALL) != None:
-        play["play_type"]="run"
+    if not processPassPlay(text,play):
+        if not processRunPlay(text,play):
+            print("meh")
 
 def processDrive(text, t1,t2):
     global PLAY_NB
